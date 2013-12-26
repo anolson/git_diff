a0fd619932a33c79d39bdc228fa19a2ecbacdc9b
Andrew Olson <aolson@geezeo.com>
2013-12-26 13:53:44 -0500

    Move class methods into Line module.

--- lib/git_diff/line.rb
+++ lib/git_diff/line.rb
@@ -1,3 +1,22 @@
 require "git_diff/line/base"
 require "git_diff/line/addition"
 require "git_diff/line/deletion"
+
+module GitDiff
+  module Line
+
+    module ClassMethods
+      def from_string(string, line_number)
+        case
+        when string.start_with?("+")
+          Addition.new(string, line_number)
+        when string.start_with?("-")
+          Deletion.new(string, line_number)
+        else
+          Base.new(string, line_number)
+        end
+      end
+    end
+    extend ClassMethods
+  end
+end
--- lib/git_diff/line/base.rb
+++ lib/git_diff/line/base.rb
@@ -1,20 +1,5 @@
 module GitDiff
   module Line
-
-    module ClassMethods
-      def from_string(string, line_number)
-        case
-        when string.start_with?("+")
-          Addition.new(string, line_number)
-        when string.start_with?("-")
-          Deletion.new(string, line_number)
-        else
-          Base.new(string, line_number)
-        end
-      end
-    end
-    extend ClassMethods
-
     class Base
       attr_reader :content, :line_number
 
